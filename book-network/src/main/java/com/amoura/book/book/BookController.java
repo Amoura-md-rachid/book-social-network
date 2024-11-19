7cec4f3724093afea0976af8f5a162a5b61fe297
m-amoura <amoura.md.rachid@gmail.com>
2024-11-19 10:31:51 +0100

    Implement save book method and find book by id

---
+++ book-network/src/main/java/com/amoura/book/book/BookController.java
@@ -0,0 +1,38 @@
+package com.amoura.book.book;
+
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("books")
+@RequiredArgsConstructor
+@Tag(name = "Book")
+public class BookController {
+
+    private BookService service;
+    private final BookMapper bookMapper;
+    private final BookRepository bookRepository;
+
+
+    @PostMapping
+    public ResponseEntity<Integer> saveBook(
+            @Valid @RequestBody BookRequest request,
+            Authentication connectedUser
+    ) {
+        return ResponseEntity.ok(service.save(request, connectedUser));
+    }
+
+    @GetMapping("/{book-id}")
+    public BookResponse findById(Integer bookId) {
+        return bookRepository.findById(bookId)
+                .map(bookMapper::toBookResponse)
+                .orElseThrow(() -> new EntityNotFoundException("No book found with ID:: " + bookId));
+    }
+}
